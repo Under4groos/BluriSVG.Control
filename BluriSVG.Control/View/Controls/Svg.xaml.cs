1050b61a430dced3ee736be4eb6bbebaa14b0f94
Макс Титов <hhit934@gmail.com>
2022-09-03 15:07:27 +0300

    asdasd

--- BluriSVG.Control/View/Controls/Svg.xaml.cs
+++ BluriSVG.Control/View/Controls/Svg.xaml.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -40,6 +42,64 @@ namespace BluriSVG.Control.View.Controls
                 OnPropertyChanged("svg_update");
             }
         }
+        Size _Resize = new Size(1,1);
+        public Size Resize
+        {
+            get
+            {
+                return _Resize;
+
+
+            }
+            set
+            {
+                _Resize = value;
+                _resize(_Resize.Width, _Resize.Height);
+            }
+        }
+        private string _path_svg = "";
+
+
+        public string PathSvg
+        {
+            get
+            {
+                return _path_svg;
+            }
+            set
+            {
+                _path_svg = value;
+                if (File.Exists(_path_svg))
+                {
+                    string data_ = File.ReadAllText(_path_svg);
+                    DataList.Clear();
+                    foreach (Match _svg in Regex.Matches(data_ , "<svg[\\w\\W]+?<\\/svg>"))
+                    {
+                        foreach (Match item in Regex.Matches(_svg.Value, "<path[\\w\\W]+?\\/>"))
+                        {
+
+                            foreach (Match _d in Regex.Matches(item.Value, "d=\\\"[\\w\\W]+?\\\""))
+                            {
+
+                                DataList.Add(Regex.Replace(_d.Value, "(d=\")|(\")", ""));
+                               // Console.WriteLine(Regex.Replace(_d.Value, "(d=\")|(\")", ""));
+                            }
+                        }
+                    }
+
+                }
+            }
+        }
+
+        private void _resize(double w , double h)
+        {
+            ScaleTransform myScaleTransform = new ScaleTransform();
+            myScaleTransform.ScaleX = w;
+            myScaleTransform.ScaleY = h;
+            TransformGroup myTransformGroup = new TransformGroup();
+            myTransformGroup.Children.Add(myScaleTransform);
+            this.RenderTransform = myTransformGroup;
+        }
         public void OnPropertyChanged([CallerMemberName] string prop = "")
         {
             if (PropertyChanged != null)
@@ -52,14 +112,6 @@ namespace BluriSVG.Control.View.Controls
         {
             InitializeComponent();
             this.DataContext = this;
-
-
-            //System.Windows.Shapes.Path path_svg = new System.Windows.Shapes.Path();
-
-            //path_svg.Data = Geometry.Parse(svg.d);
-            //path_svg.Fill = Brushes.White;
-
-            //path_svg.SnapsToDevicePixels = true;
         }
     }
 }
---
+++ BluriSVG.TestApplication/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BluriSVG.TestApplication
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += (o, e) =>
+            {
+                svg_.DataList.Add("M101.36,45A6.65,6.65,0,0,1,108,51.64V85.36A6.65,6.65,0,0,1,101.36,92H12.64A6.65,6.65,0,0,1,6,85.36V51.64A6.65,6.65,0,0,1,12.64,45h88.72m0-6H12.64A12.64,12.64,0,0,0,0,51.64V85.36A12.64,12.64,0,0,0,12.64,98h88.72A12.64,12.64,0,0,0,114,85.36V51.64A12.64,12.64,0,0,0,101.36,39Z");
+            };
+        }
+    }
+}
