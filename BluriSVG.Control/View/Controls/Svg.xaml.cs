76e853c9b95b1a3dc05b41f8f1759c9c99dcd295
Макс Титов <hhit934@gmail.com>
2022-09-03 14:50:26 +0300

    asd

--- BluriSVG.Control/View/Controls/Svg.xaml.cs
+++ BluriSVG.Control/View/Controls/Svg.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,11 +21,45 @@ namespace BluriSVG.Control.View.Controls
     /// <summary>
     /// Логика взаимодействия для Svg.xaml
     /// </summary>
-    public partial class Svg : UserControl
+    public partial class Svg : UserControl , INotifyPropertyChanged
     {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private ObservableCollection<string> DataList_ = new ObservableCollection<string>();
+
+
+        public ObservableCollection<string> DataList
+        {
+            get
+            {
+                return DataList_;
+            }
+            set
+            {
+                DataList_ = value;
+                OnPropertyChanged("svg_update");
+            }
+        }
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
+
+
         public Svg()
         {
             InitializeComponent();
+            this.DataContext = this;
+
+
+            //System.Windows.Shapes.Path path_svg = new System.Windows.Shapes.Path();
+
+            //path_svg.Data = Geometry.Parse(svg.d);
+            //path_svg.Fill = Brushes.White;
+
+            //path_svg.SnapsToDevicePixels = true;
         }
     }
 }
